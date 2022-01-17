f0b1da238fc9397537589c212bee2db41913f403
QuieresSerMiNovia <97859064+QuieresSerMiNovia@users.noreply.github.com>
2022-01-16 18:50:09 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+const yesBtn = document.querySelector('#yesBtn');
+const noBtn = document.querySelector('#noBtn');
+
+yesBtn.addEventListener('click', function () {
+    alert("Pues yo no :v");
+})
+
+noBtn.addEventListener('mouseover', function () {
+    const randomX = parseInt(Math.random() * 100);
+    const randomY = parseInt(Math.random() * 100);
+    noBtn.style.setProperty('top', randomY + '%');
+    noBtn.style.setProperty('left', randomX + '%');
+    noBtn.style.setProperty('transform', `traslate(-${randomX}%,-${randomY}%)`);
+})
+
+noBtn.addEventListener('click', function () {
+    alert("No pues...");
+    if (alert) {
+        alert("Dinosaurio no?");
+    }
+})
